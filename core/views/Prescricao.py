bdadece9ac46cb1d271c8507200ddc0bfcb95ac6
anapauladominoni <anapauladom29@gmail.com>
2026-08-17 14:32:37 -0300

    feat: adicionar campos ao residente

--- core/views/Prescricao.py
+++ core/views/Prescricao.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from core.models import Prescricao
 from core.serializers import PrescricaoSerializer
 
+
 class PrescricaoViewSet(ModelViewSet):
     queryset = Prescricao.objects.all()
     serializer_class = PrescricaoSerializer
--- core/views/Residente.py
+++ core/views/Residente.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from core.models import Residente
 from core.serializers import ResidenteSerializer
 
+
 class ResidenteViewSet(ModelViewSet):
     queryset = Residente.objects.all()
     serializer_class = ResidenteSerializer
